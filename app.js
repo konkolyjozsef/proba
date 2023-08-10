0cda90f696135488ce60d9114a5554952bb5f200
konkolyjozsef <konkoly.joci02@gmail.com>
2023-08-10 08:35:49 +0200

    numbercounters done

--- app.js
+++ app.js
@@ -13,3 +13,28 @@ document.addEventListener("DOMContentLoaded", function() {
 
   const dateDisplay= document.getElementById('date-container');
   dateDisplay.innerHTML=currentTime();
+
+
+
+/*https://codepen.io/akhijannat/pen/JjYQgNK*/
+
+const counters = document.querySelectorAll('.value');
+const speed = 400;
+
+counters.forEach( counter => {
+   const animate = () => {
+      const value = +counter.getAttribute('datvalue');
+      const data = +counter.innerText;
+     
+      const time = value / speed;
+     if(data < value) {
+          counter.innerText = Math.ceil(data + time);
+          setTimeout(animate, 1);
+        }else{
+          counter.innerText = value;
+        }
+     
+   }
+   
+   animate();
+});
